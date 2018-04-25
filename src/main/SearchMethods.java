587189b5a846722e416f1eb55f89788940e84046
Viorel <Viorel@Viorel-PC>
2018-04-26 00:39:44 +0300

    fixed project

--- src/main/SearchMethods.java
+++ src/main/SearchMethods.java
@@ -1,4 +1,4 @@
-
+package main;
 
 public final class SearchMethods {
 
---
+++ src/main/Tests.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.junit.Test;
+
+public final class Tests {
+	
+	public Tests() {
+		super();
+	}
+	
+	/**
+	 * Binary Search Functional Test Implementation
+	 */
+	@Test
+	public static void functionalTest() {
+		
+	}
+	
+	
+	/**
+	 * Binary Search Structural Test Implementation
+	 */
+	@Test
+	public static void structuralTest() {
+		
+	}
+	
+	public static void main(String[] args) {
+		SearchMethods.binarySearch(0, new int[]{0, 1 ,2 ,3});
+	}
+}
